5dd2dabd79cbb880f53c389f11a932fde2822370
Kumarshl <sahilkumarcssoftsolutions.com>
2023-08-22 18:26:16 +0530

    test

--- src/app/_services/importexport.service.ts
+++ src/app/_services/importexport.service.ts
@@ -9,6 +9,7 @@ export class ImportexportService {
   private baseApiUrl = 'https://localhost:44380';
   constructor(private http: HttpClient) {}
   uploadExcelFile(file: any): Observable<any> {
+    
     const formData = new FormData();
     formData.append('excelFile', file);
     return this.http.post(this.baseApiUrl + 'api/upload', formData);
